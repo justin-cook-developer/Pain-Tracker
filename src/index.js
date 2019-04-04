0f785f22d96779834db1ad13c3f083aafea53e3c
Justin Cook <justin.cook.dev@gmail.com>
2019-04-04 14:30:27 -0700

    Initial server setup and webpack set up.

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'bulma/css/bulma.css'
+
+const App = () => <div className='is-centered'><button type="button" className='button is-primary'>App</button></div>
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+)
+
---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: ['@babel/polyfill', './src/index.js'],
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public'),
+  },
+  devtool: 'source-map',
+  devServer: {
+    contentBase: path.join(__dirname, 'public'),
+    compress: true,
+    port: 8080
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: 'babel-loader',
+      },
+    ],
+  },
+};
