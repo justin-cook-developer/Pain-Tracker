98f4a4172b6ddb342754d4c49ee58a517ad77c06
Justin Cook <justin.cook.dev@gmail.com>
2019-04-26 17:09:15 -0700

    Edit form working

--- src/components/record/Record.js
+++ src/components/record/Record.js
@@ -4,22 +4,23 @@ import { connect } from 'react-redux';
 import { deleteRecord } from '../../actions/records';
 import RecordDumb from './RecordDumb';
 
-const Record = ({ record, removeRecord }) => (
-  <RecordDumb record={record} handleClick={removeRecord} />
+const Record = ({ record, removeRecord, pageNumber }) => (
+  <RecordDumb record={record} handleClick={removeRecord(pageNumber)} />
 );
 
-const mapStateToProps = ({ records }, { match }) => {
-  const id = parseInt(match.params.id);
+const mapStateToProps = ({ records, allRecordsUI: ui }, { match }) => {
+  const id = parseInt(match.params.id)
+  const { pageNumber } = ui
   const record = records.find(record => record.id === id);
-  return { record };
+  return { record, pageNumber };
 };
 
 const mapDispatchToProps = (dispatch, { history, match }) => ({
-  removeRecord: () => {
-    const id = parseInt(match.params.id);
-    const action = deleteRecord(id);
+  removeRecord: pageNumber => () => {
+    const id = parseInt(match.params.id)
+    const action = deleteRecord(id)
     dispatch(action);
-    history.push('/records');
+    history.push(`/records/${pageNumber}`);
   },
 });
 
